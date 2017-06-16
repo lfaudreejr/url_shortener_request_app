5cfeb91d6f78448ffc2069c60f93e10ceac00212
Larry Faudree Jr <lfaudreejr@hotmail.com>
2017-06-16 10:08:37 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {};
 
 config.db = {};
 
-config.db.host = 'user:pass@ds155201.mlab.com:55201';
+config.db.host = 'user info';
 config.db.name = 'url-short-service';
 
 module.exports = config;
